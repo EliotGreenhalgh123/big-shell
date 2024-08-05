2a6638453920893e82051dbd9b072df3b9b8aa7e
Eliot Greenhalgh <97060609+EliotGreenhalgh123@users.noreply.github.com>
2024-08-05 20:43:03 +0000

    -implemented signal restore, signal ignore, & signal interrupt functionality

--- src/signal.c
+++ src/signal.c
@@ -1,6 +1,7 @@
 #define _POSIX_C_SOURCE 200809L
 #include <signal.h>
 #include <errno.h>
+#include <stdio.h>
 
 #include "signal.h"
 
@@ -66,9 +67,16 @@ signal_init(void)
 int
 signal_enable_interrupt(int sig)
 {
-  /* TODO set the signal disposition for signal to interrupt  */
-  errno = ENOSYS; /* not implemented */
-  return -1;
+  /* DONE set the signal disposition for signal to interrupt  */
+  /* 
+   * assign the signal with interrupt action 
+   * NULL pointer in place of oact indicates old signal disposition will not be saved
+   */
+  if (sigaction(sig, &interrupt_action, NULL) != 0) {
+    return -1;
+  }
+  //errno = ENOSYS; /* not implemented */
+  return 0;
 }
 
 /** ignore a signal
@@ -80,9 +88,12 @@ signal_enable_interrupt(int sig)
 int
 signal_ignore(int sig)
 {
-  /* TODO set the signal disposition for signal back to its old state */
-  errno = ENOSYS; /* not implemented */
-  return -1;
+  /* DONE set the signal disposition for signal back to its old state */
+  if (sigaction(sig, &signal_ignore, NULL) != 0) {
+    return -1;
+  }
+  //errno = ENOSYS; /* not implemented */
+  return 0;
 }
 
 /** Restores signal dispositions to what they were when bigshell was invoked
@@ -93,11 +104,20 @@ signal_ignore(int sig)
 int
 signal_restore(void)
 {
-  /* TODO restore old actions 
+  /* DONE restore old actions 
    *
    * e.g. sigaction(SIGNUM, &saved_old_handler, NULL);
    *
    * */
-  errno = ENOSYS; /* not implemented */
-  return -1;
+  if (sigaction(SIGTSTP, &old_sigtstp, NULL) != 0) {
+    return -1;
+  }
+  if (sigaction(SIGINT, &old_sigint, NULL) != 0) {
+    return -1;
+  }
+  if (sigaction(SIGTTOU, &old_sigttou, NULL) != 0) {
+    return -1;
+  }
+  //errno = ENOSYS; /* not implemented */
+  return 0;
 }
