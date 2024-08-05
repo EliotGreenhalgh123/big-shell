e847b94095e26f5d4c0bdb9258db2f0c7d76cc43
Eliot Greenhalgh <97060609+EliotGreenhalgh123@users.noreply.github.com>
2024-08-05 20:43:42 +0000

    -uncommented all calls to signal functions in main loop

--- src/bigshell.c
+++ src/bigshell.c
@@ -38,12 +38,12 @@ prompt:
     /* Read input and parse it into a list of commands */
     
     /* TODO Enable this line once you've implemented the function */
-    //if (signal_enable_interrupt(SIGINT) < 0) goto err;
+    if (signal_enable_interrupt(SIGINT) < 0) goto err;
     
     int res = command_list_parse(&cl, stdin);
     
     /* TODO Enable this line once you've implemented the function */
-    // if (signal_ignore(SIGINT) < 0) goto err;
+    if (signal_ignore(SIGINT) < 0) goto err;
 
     if (res == -1) { /* System library errors */
       switch (errno) { /* Handle specific errors */
